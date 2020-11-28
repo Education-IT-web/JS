f6cee329b7a024c4aa09938cf71233975ca5a887
Oleg Kochiev <developer-artisan@mail.ru>
2020-11-28 14:42:01 +0300

    was completed project 'object_to_primitive'

---
+++ Objects/object_to_primitive/script.js
@@ -0,0 +1,48 @@
+"use strict"
+
+
+function Animal(name, type) {
+  this.name = name;
+  this.type = type;
+}
+
+let dog = new Animal("Sharik", "dog");
+//console.log(dog);
+
+//console.log(Animal);
+
+
+let user = {
+  name: "John",
+  money: 1000,
+
+  [Symbol.toPrimitive](hint) {
+    console.log(`hint: ${hint}`);
+    return hint == "string" ? `{name: "${this.name}"}` : this.money;
+  }
+};
+
+// демонстрация результатов преобразований:
+console.log(user); // hint: string -> {name: "John"}
+console.log(+user); // hint: number -> 1000
+console.log(user + 500); // hint: default -> 1500
+// То же самое что и предыдущее, только выполнено с помощью методов объекта
+let user = {
+  name: "John",
+  money: 1000,
+
+  // для хинта равного "string"
+  toString() {
+    return `{name: "${this.name}"}`;
+  },
+
+  // для хинта равного "number" или "default"
+  valueOf() {
+    return this.money;
+  }
+
+};
+
+alert(user); // toString -> {name: "John"}
+alert(+user); // valueOf -> 1000
+alert(user + 500); // valueOf -> 1500
